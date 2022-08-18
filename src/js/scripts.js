c8835cef59ec035a9bfc67f967c6ab3d34d43f94
KirylBarokha <105718467+KirylBarokha@users.noreply.github.com>
2022-08-18 15:40:00 +0300

    add hero block

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -9,3 +9,23 @@ if (burger && menu) {
     body.classList.toggle('_lock')
   })
 }
+
+//filter dropdown
+
+const filter = document.querySelector('.filter')
+
+if (filter) {
+  const items = filter.querySelectorAll('.block-filter')
+
+  items.forEach((item) => {
+    item.addEventListener('click', (event) => {
+      item.querySelector('.block-filter__dropdawn').classList.toggle('_active')
+      item.querySelector('.block-filter__icon').classList.toggle('_active')
+
+      if (event.target.classList.contains('block-filter__item')) {
+        item.querySelector('.block-filter__value').textContent =
+          event.target.textContent
+      }
+    })
+  })
+}
